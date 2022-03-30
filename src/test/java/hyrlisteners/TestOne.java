14d08d8eeb596126197f877afcf99abf34ce0119
sridevi <sureshkadasulaseo@gmail.com>
2022-03-30 11:17:55 +0530

    Commiting testOne to BranchX

---
+++ src/test/java/hyrlisteners/TestOne.java
@@ -0,0 +1,10 @@
+package hyrlisteners;
+
+public class TestOne {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is local repository");
+	}
+
+}
